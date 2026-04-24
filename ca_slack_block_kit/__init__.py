2a278edd3f3632b49708fcf6d30cc4f0b6dccb42
Matthew Cane <matthew.cane@citizensadvice.org.uk>
2026-04-24 10:50:03 +0100

    Fix import errors

--- ca_slack_block_kit/__init__.py
+++ ca_slack_block_kit/__init__.py
@@ -11,9 +11,28 @@ required by the Slack API.
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
-from .emoji import Emoji  # type: ignore[import]
 
-__all__ = ["Emoji"]
+@dataclass(frozen=True)
+class Emoji:
+    """Various emojis for easy use with Slack messages."""
+
+    THUMBS_UP = ":thumbsup:"
+    THUMBS_DOWN = ":thumbsdown:"
+    PARTY_POPPER = ":tada:"
+    WARNING = ":warning:"
+    CHECK_MARK = ":white_check_mark:"
+    CROSS_MARK = ":x:"
+    INFO = ":information_source:"
+    ROCKET = ":rocket:"
+    EYES = ":eyes:"
+    FIRE = ":fire:"
+    STAR = ":star:"
+    HEART = ":heart:"
+    CLAP = ":clap:"
+    LIGHT_BULB = ":bulb:"
+    HOURGLASS = ":hourglass_flowing_sand:"
+    BUG = ":bug:"
+    SPARKLES = ":sparkles:"
 
 
 def render_blocks(blocks: list["Block"]) -> list[dict]:
